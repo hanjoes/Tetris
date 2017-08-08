986302c91d5f7bec7f887e252f827d50c175dcfb
Hanzhou Shi <hanzhou87@gmail.com>
2017-08-07 22:51:20 -0700

    Setup skeleton project.

---
+++ Tetris/Tetris/GameScene.swift
@@ -0,0 +1,34 @@
+//
+//  GameScene.swift
+//  Tetris
+//
+//  Created by Hanzhou Shi on 8/7/17.
+//  Copyright © 2017 Hanzhou Shi. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
