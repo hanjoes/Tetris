f2d7606efa521b7b2f650a3f2dd8e22136545f34
Hanzhou Shi <hanzhou87@gmail.com>
2017-08-10 22:53:22 -0700

    Implemented sorting for points inside Polyominoes.

--- Tetris/Tetris/Polyomino.swift
+++ Tetris/Tetris/Polyomino.swift
@@ -10,23 +10,55 @@ struct Polyomino {
     /// Points defines this polyomino.
     /// The points represents the bottom-left corner of
     /// the cells.
-    var points: [CGPoint]
+    /// - Note: The points are always sorted. see `sorter` for detail.
+    let points: [CGPoint]
+    
+    /// Sorts two points. A point to the left of another point appears
+    /// before. A point above another point appears before.
+    ///
+    ///
+    /// - Note: Direction in this function follows convention used in
+    /// _SpriteKit_ so, positive direction for y axis grows from
+    /// bottom of the screen to up of the screen.
+    /// - Parameters:
+    ///   - p1: one `CGPoint`
+    ///   - p2: another `CGPoint`
+    /// - Returns: boolean indicates whether `p1` goes before `p2`
+    static func sorter(p1: CGPoint, p2: CGPoint) -> Bool {
+        if p1.x < p2.x {
+            return true
+        }
+        
+        if p1.x == p2.x {
+            return p1.y > p2.y
+        }
+
+        return false
+    }
     
     /// Create a `Polyomino` by a list of points
     ///
     /// - Parameter points: the list of points defines a `Polyomino`
     init(fromPoints points: [CGPoint]) {
-        self.points = points
+        self.points = points.sorted(by: Polyomino.sorter)
     }
     
+    /// Normalize is to translate the polyomino so that the first
+    /// point sits at the origin.
+    ///
+    /// - Returns: a new `Polyomino` normalized
     func normalized() -> Polyomino {
         return self
     }
     
+    /// Rotates a polyomino clockwise by 90 degrees.
+    ///
+    /// - Returns: The clockwise-rotated polyomino.
     func clockwiseRotated() -> Polyomino {
         return self
     }
 }
+
 
 // MARK: - Hashable
 extension Polyomino: Hashable {
--- Tetris/TetrisTests/TetrisTests.swift
+++ Tetris/TetrisTests/TetrisTests.swift
@@ -11,26 +11,14 @@ import XCTest
 
 class TetrisTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testHorizontalSorted() {
+        let polyomino = Polyomino(fromPoints: [CGPoint(x: 1, y: 0), CGPoint(x: 0, y: 0)])
+        XCTAssertEqual(polyomino.points, [CGPoint(x: 0, y: 0), CGPoint(x: 1, y: 0)])
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testVerticalSorted() {
+        let polyomino = Polyomino(fromPoints: [CGPoint(x: 1, y: 0), CGPoint(x: 1, y: 1)])
+        XCTAssertEqual(polyomino.points, [CGPoint(x: 1, y: 1), CGPoint(x: 1, y: 0)])
     }
     
 }
